db60e841c1851f58b038681c9305be5e0dafc83d
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-06-18 10:54:25 -0300

    Secao 21|Aula 278 - Implementacao da Classe Seller|Projeto DEMO-DAO-JDBC Criado|CONCLUIDO

--- demo-dao-jdbc/src/application/program.java
+++ demo-dao-jdbc/src/application/program.java
@@ -1,14 +1,28 @@
 package application;
 
+import java.time.Instant;
+import java.util.Date;
+
+import model.entities.Seller;
 import model.entities.Department;
 
 public class program {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Tecnologia da Informação");
+		Department department = new Department(1, "Tecnologia da Informação");		
+		//System.out.println(department);
 		
-		System.out.println(department);
+		Seller seller = new Seller (
+				1, //Integer id, 
+				"Carlos Eduardo dos Santos Roberto", //String name, 
+				"cadu172@gmail.com", //String email, 
+				Date.from(Instant.parse("1981-09-17T00:00:00Z")), //Date birthDate, 
+				5600.00, //Double baseSalary
+				department
+				);
+		
+		System.out.println(seller);
 				
 
 	}
