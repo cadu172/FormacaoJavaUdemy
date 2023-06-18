ebee24c6724cfb049187ae8df0916ff4f7dcd03b
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-06-18 10:36:12 -0300

    Secao 21|Aula 277 - |Implementacao da Classe Department|Projeto DEMO-DAO-JDBC Criado|CONCLUIDO

--- demo-dao-jdbc/src/application/program.java
+++ demo-dao-jdbc/src/application/program.java
@@ -1,9 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class program {
 
 	public static void main(String[] args) {
 		
+		Department department = new Department(1, "Tecnologia da Informação");
+		
+		System.out.println(department);
 				
 
 	}
