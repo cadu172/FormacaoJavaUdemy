c379e7fe16876560b97c63bd0b5c964ea1d8ebd8
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-06-23 20:18:43 -0300

    Seção 22|Aula 294 - Nivelamento JPA / Hibernate - PARTE 3 (Trabalhando com Maven)|CONCLUIDO

--- AulaJPAMaven/src/main/java/aplicacao/Programa.java
+++ AulaJPAMaven/src/main/java/aplicacao/Programa.java
@@ -1,5 +1,11 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.boot.model.source.internal.hbm.CompositeIdentifierSingularAttributeSourceManyToOneImpl;
+
 import dominio.Pessoa;
 
 public class Programa {
@@ -7,13 +13,35 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		
-		Pessoa p1 = new Pessoa(1,"Carlos Eduardo","cadu172@gmail.com");
-		Pessoa p2 = new Pessoa(2,"Marco Antonio Silva","marco@gmail.com");
-		Pessoa p3 = new Pessoa(3,"Ronaldo Silva","r9@gmail.com");
+		/*Pessoa p1 = new Pessoa(null,"Carlos Eduardo","cadu172@gmail.com");
+		Pessoa p2 = new Pessoa(null,"Marco Antonio Silva","marco@gmail.com");
+		Pessoa p3 = new Pessoa(null,"Ronaldo Silva","r9@gmail.com");*/
 		
-		System.out.println(p1);
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager manager = factory.createEntityManager();
+		
+		/*
+		 * Criando registro no banco usando ORM
+		manager.getTransaction().begin();
+		manager.persist(p1);
+		manager.persist(p2);
+		manager.persist(p3);
+		manager.getTransaction().commit();*/
+		
+		// localizando uma pessoal usando o find (obs: esta INSTANCIA esta sendo monitorada)
+		Pessoa p1 = manager.find(Pessoa.class, 1);
+		
+		manager.getTransaction().begin();
+		manager.remove(p1);
+		manager.getTransaction().commit();
+		
+		//System.out.println(p1);
+		
+		System.out.println("Finish...");
+		
+		/*System.out.println(p1);
 		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println(p3);*/
 
 	}
 
--- AulaJPAMaven/src/main/java/dominio/Pessoa.java
+++ AulaJPAMaven/src/main/java/dominio/Pessoa.java
@@ -1,11 +1,20 @@
 package dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements java.io.Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	private String email;
 
