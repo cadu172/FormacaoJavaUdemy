a0cfeea4e1e1474c5e4fbc4677268d1ce30870a9
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-06-30 00:09:02 -0300

    308. Associação muitos-para-muitos com JoinTable

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -41,14 +41,20 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");	
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));		
 		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
