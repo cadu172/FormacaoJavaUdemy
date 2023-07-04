b1a99d3c592004f0e0a79ba3a2ecdf0dfcc5e239
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-07-03 23:17:40 -0300

    continuar na aula 310

--- course/src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ course/src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -15,7 +15,7 @@ public class OrderItem  implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	private Integer quantity;
 	private Double price;
 	
