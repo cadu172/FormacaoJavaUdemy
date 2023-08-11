6a9ce409f7b031f6257103999ad8d03340ff5eea
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-08-11 20:19:44 -0300

    314. Inserção de User

--- course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -43,7 +45,9 @@ public class UserResource {
 		
 		user = service.insert(user);
 		
-		return ResponseEntity.ok().body(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(location).body(user);
 		
 	}
 	
