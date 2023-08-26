947da9e5ceec1701fdc29e913e0c2aafeeba3247
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-08-26 11:33:12 -0300

    Inclusao do Projeto Spring com MongoDB - Projeto WorkShopMongoDB

---
+++ workshopmongo/src/test/java/com/cadu172/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadu172.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
