1389f1f7db408c37bc412fc7907ac66b8d0d9964
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-08-28 20:33:12 -0300

    Seção 24| 343. Deleção de usuário com DELETE

--- workshopmongo/src/main/java/com/cadu172/workshopmongo/resource/UserResource.java
+++ workshopmongo/src/main/java/com/cadu172/workshopmongo/resource/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,12 @@ public class UserResource {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		
 		return ResponseEntity.created(location).body(user);		
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deleteByid(@PathVariable String id) {
+		userService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- workshopmongo/src/main/java/com/cadu172/workshopmongo/service/UserService.java
+++ workshopmongo/src/main/java/com/cadu172/workshopmongo/service/UserService.java
@@ -43,4 +43,8 @@ public class UserService {
 		
 	}
 	
+	public void deleteById(String id) {
+		userRepository.deleteById(id);		
+	}
+	
 }
