10236d255536ac5cb56b090abe8c7c56eafb78bb
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-09-03 10:33:57 -0300

    Seção 24| 347. Projeção dos dados do autor com DTO

--- workshopmongo/src/main/java/com/cadu172/workshopmongo/config/DatabaseSeed.java
+++ workshopmongo/src/main/java/com/cadu172/workshopmongo/config/DatabaseSeed.java
@@ -2,7 +2,6 @@ package com.cadu172.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.cadu172.workshopmongo.domain.Post;
 import com.cadu172.workshopmongo.domain.User;
+import com.cadu172.workshopmongo.dto.AuthorDTO;
 import com.cadu172.workshopmongo.repository.PostRepository;
 import com.cadu172.workshopmongo.repository.UserRepository;
 
@@ -35,14 +35,14 @@ public class DatabaseSeed implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		User user1 = new User(null, "Maria Brown", "maria@gmail.com");		
-		User user2 = new User(null, "Alex Green", "alex@gmail.com");
-		User user3 = new User(null, "carlos", "cadu172@gmail.com");		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");		
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User carlos = new User(null, "carlos", "cadu172@gmail.com");		
 		
-		userRepository.saveAll(Arrays.asList(user1, user2, user3));
+		userRepository.saveAll(Arrays.asList(maria, alex, carlos));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", user1);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", user1);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
