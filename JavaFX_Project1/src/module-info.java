9d6cf2925d7c7a2e36d5aff9f9162b5c4ed6fc08
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-09-19 11:29:01 -0300

    Seção 25| 361. Testando o FXML

---
+++ JavaFX_Project1/src/module-info.java
@@ -0,0 +1,6 @@
+module JavaFX_Project1 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
--- ProjetoBasicoJavaFX/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module ProjetoBasicoJAVAFX {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
