04403b006ba2f24b155c2d38e2cb947ae0b60cb3
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-09-26 20:46:11 -0300

    Seção 25| 364. Usando TextField e Label (app para calcular soma)

--- JavaFX_Project3/src/gui/ViewController.java
+++ JavaFX_Project3/src/gui/ViewController.java
@@ -1,18 +1,51 @@
 package gui;
 
+import java.util.Locale;
+
 import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 	
 	@FXML
-	private Button BtnTeste;
+	private TextField EdtValor1;
+	
+	@FXML
+	private TextField EdtValor2;
+
+	@FXML
+	private Button BtnSoma;
+	
+	@FXML
+	private Label lblResultado;
 	
 	@FXML
 	public void BtnTeste_OnAction() {
-		Alerts.showAlert("Titulo do alerta", "Header do Alerta", "Conteúdo da mensagem", AlertType.WARNING);
+		
+		Locale.setDefault(Locale.US);
+		
+		try {
+			
+			// recebe os valores informados pelo usuário
+			float valorCampo1 = Float.parseFloat(EdtValor1.getText());
+			float valorCampo2 = Float.parseFloat(EdtValor2.getText());
+			
+			// realiza asoma
+			float somaValor = valorCampo1 + valorCampo2;		
+
+			// transfere o resultado para o label na tela
+			lblResultado.setText(String.format("%.2f", somaValor));
+
+		
+		}
+		catch (NumberFormatException e) {
+			Alerts.showAlert("Erro de Conversão", "Número Informado Incorretamente", e.getMessage(), AlertType.WARNING);
+		}
+		
 	}
 	
 }
