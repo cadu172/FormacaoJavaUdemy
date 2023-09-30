7d6a5147326b3fcb9a6dc2d4d220224f3bb909d1
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-09-30 00:42:36 -0300

    Seção 25| 373. About view

--- workshop_java_jdbc/src/application/Main.java
+++ workshop_java_jdbc/src/application/Main.java
@@ -9,6 +9,12 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -19,7 +25,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);			
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
