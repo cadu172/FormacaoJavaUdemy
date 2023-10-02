fe8659340c47c6502d750e013c2dfb05ec33d1ee
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-10-02 20:14:35 -0300

    Seção 25| 374. DepartmentList view design

--- workshop_java_jdbc/src/gui/MainViewController.java
+++ workshop_java_jdbc/src/gui/MainViewController.java
@@ -40,7 +40,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
