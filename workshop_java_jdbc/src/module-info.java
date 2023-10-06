be2431b1a1230f1fe61377796024c98cb64110f6
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2023-10-06 00:30:06 -0300

    Commit local

--- workshop_java_jdbc/src/module-info.java
+++ workshop_java_jdbc/src/module-info.java
@@ -1,9 +1,9 @@
 module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+	/*
 	exports gui;
 	opens gui;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml;*/
 }
